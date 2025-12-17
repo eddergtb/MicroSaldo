28f4fc1bee145f03fd32f4ad5ecc966ad0679ade
Reck <dylanmorenovargas40@gmail.com>
2025-12-16 19:11:30 -0500

    Agregué vistas, layouts con sidebar, JS de la sidebar y estilos

---
+++ MicroSaldoFinal/wwwroot/js/sidebar.js
@@ -0,0 +1,6 @@
+﻿const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menu_btn');
+
+    menuBtn.addEventListener('click', () => {
+        sidebar.classList.toggle('minimize');
+    });
